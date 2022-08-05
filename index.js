5ba69dfe3f9aea5bfddd207e910c2223c1a18b25
Owen Ludlam <oludlam@umass.edu>
2022-08-05 15:50:00 -0400

    Form Alterations > Changed Order of inputs > Added description > Added Details section > Added Location dropdown > Dynamically create location options > Added seats input > Added Tables input > Added Wall Ports input > Added CSS for the offcanvas for a more readable form

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
+/**
+ * Live-display the chosen image
+ */
 const image_input = document.getElementById('image_input');
 let uploaded_image = '';
 
-image_input.addEventListener('change', () => {
+image_input.addEventListener('change', function () {
     const reader = new FileReader();
     reader.addEventListener('load', () => {
         uploaded_image = reader.result;
         document.getElementById('display_image').style.backgroundImage = `url(${uploaded_image})`;
     });
-    reader.readAsDataURL(image_input.files[0]);
+    reader.readAsDataURL(this.files[0]);
 });
+
+/**
+ * Get viable locations (Will be loaded from server in the future)
+ */
+function makeLocationList() {
+    let locations = [ //Replace with GET
+        'A',
+        'B',
+        'C',
+        'D',
+        'E',
+        'F'
+    ];
+    const dropdown = document.getElementById('location-dropdown-list');
+    const choice = document.getElementById('location-location');
+    locations.forEach(l => {
+        let li = document.createElement('li');
+        let div = document.createElement('div');
+        div.classList.add('dropdown-item');
+        div.innerText = l;
+        li.onclick = () => {
+            choice.value = l;
+        }
+        li.appendChild(div);
+        dropdown.appendChild(li);
+    });
+}
+
+window.onload = () => {
+    makeLocationList;
+}
